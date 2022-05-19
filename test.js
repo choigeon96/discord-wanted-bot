6c75c437082bf6f612fcbc9a4842e47f7894a12f
choigeon <ck731@naver.com>
2022-05-19 14:04:17 +0900

    location 지역만

--- test.js
+++ test.js
@@ -25,7 +25,10 @@ async function crawler() {
     }
   })
   page.close()
-
+  if (!positions[0].position) {
+    console.error("undefined")
+    return
+  }
   for (let i = 0; i < positions.length; i++) {
     const id = positions[i].id.split("/")[2]
     const url = `https://www.wanted.co.kr/wd/${id}`
@@ -36,11 +39,9 @@ async function crawler() {
     await detailPage.goto(url)
     detailPage.waitFor(3000)
     let detail = await detailPage.content()
-    const $detail = cheerio.load(detail, { decodeEntities: true })
-    const $detailBody = $detail(
-      ".JobDescription_JobDescription__VWfcb"
-    ).children("p")
-    const res = sanitizeHtml($detailBody, {
+    const $Job = cheerio.load(detail, { decodeEntities: true })
+    const $JobBody = $Job(".JobDescription_JobDescription__VWfcb").children("p")
+    const res = sanitizeHtml($JobBody, {
       parser: {
         decodeEntities: true,
       },
@@ -49,6 +50,7 @@ async function crawler() {
       .split("</p>")
       .map((el) => el.replace("<p><span>", ""))
 
+    const location = positions[i].location.split(".")[0]
     const task = descriptions[1].split("<br />").join("\r\n").replace("</span>")
     const required = descriptions[2]
       .split("<br />")
@@ -59,8 +61,27 @@ async function crawler() {
       .join("\r\n")
       .replace("</span>")
     detailPage.close()
-    positions[i] = { ...positions[i], id, url, task, required, preferred }
+    console.log({
+      ...positions[i],
+      id,
+      url,
+      task,
+      required,
+      preferred,
+      location,
+    })
+    positions[i] = {
+      ...positions[i],
+      id,
+      url,
+      task,
+      required,
+      preferred,
+      location,
+    }
   }
+
+  return positions
 }
 
 crawler()
